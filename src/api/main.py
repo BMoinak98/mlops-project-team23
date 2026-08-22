ac1ddc48d01eb434187333aba8e6f488c76748bb
Moinak Bandyopadhyay <moinakbanerjee98@gmail.com>
2026-08-22 12:28:08 +0530

    update code for get api

--- src/api/main.py
+++ src/api/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World! This is team 23"}
+    return {"message": "Hello World! This is team 23. Code update without docker interference"}
